5dd5961d69e5a87c7e061c53c6418894c8f7d111
grfeng4113 <gr4113@bu.edu>
2025-03-03 08:19:53 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import App from "./App";
 import { AudioProvider } from "./components/AudioContext"; 
 
 const root = createRoot(document.getElementById("root"));
+
+document.title = "Long Audio Player";
+
 root.render(
     <React.StrictMode>
         <AudioProvider>
